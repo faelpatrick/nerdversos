cf430d23e79847e756c8eb6b35165d7e355c8ce1
Rafael Patrick de Souza <45822152+faelpatrick@users.noreply.github.com>
2025-03-18 10:09:03 +0000

    logo

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
       <img src="/public/logo01.webp" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
@@ -20,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
+  border-radius: 50%;
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
